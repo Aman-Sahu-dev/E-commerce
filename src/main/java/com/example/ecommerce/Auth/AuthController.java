378d08e2341af94bf31f6eba0b135efa8010144d
Aman Sahu <amansahu3369@gmail.com>
2026-05-01 18:33:30 +0530

    security completed

--- src/main/java/com/example/ecommerce/Auth/AuthController.java
+++ src/main/java/com/example/ecommerce/Auth/AuthController.java
@@ -1,4 +1,26 @@
 package com.example.ecommerce.Auth;
 
+import com.example.ecommerce.Auth.dto.AuthResponse;
+import com.example.ecommerce.Auth.dto.LoginRequest;
+import com.example.ecommerce.Auth.dto.RegisterRequest;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthController {
+    private final AuthService authService;
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@Valid @RequestBody RegisterRequest request){
+        return ResponseEntity.status(201).body(authService.register(request));
+    }
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@Valid @RequestBody LoginRequest request){
+        return ResponseEntity.status(201).body(authService.login(request));
+    }
+
 }
--- src/main/java/com/example/ecommerce/Security/SecurityConfig.java
+++ src/main/java/com/example/ecommerce/Security/SecurityConfig.java
@@ -1,17 +1,42 @@
 package com.example.ecommerce.Security;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Component;
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
     private JwtFilter jwtFilter;
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer(){
+        return( web)-> web.ignoring().requestMatchers("/","/index.html", "/static/**", "/css/**", "/js/**");
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http
+                .csrf( csrf-> csrf.disable())
+                .authorizeHttpRequests( auth-> auth
+                        .requestMatchers("/", "/index.html", "/login.html", "/register.html", "/style.css", "/app.js").permitAll()
+                        .requestMatchers("/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+    @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
 }
+
