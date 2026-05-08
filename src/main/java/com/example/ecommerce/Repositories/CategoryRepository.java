a14e0d4e57698c76a199fb9bbcfdac878f65fe04
Aman Sahu <amansahu3369@gmail.com>
2026-05-08 20:04:58 +0530

    made product service

--- src/main/java/com/example/ecommerce/Repositories/CategoryRepository.java
+++ src/main/java/com/example/ecommerce/Repositories/CategoryRepository.java
@@ -3,5 +3,5 @@ package com.example.ecommerce.Repositories;
 import com.example.ecommerce.Model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CategoryRepository extends JpaRepository<Category,Long> {
+public interface CategoryRepository extends JpaRepository<Category,Integer> {
 }
--- src/main/java/com/example/ecommerce/Service/ProductService.java
+++ src/main/java/com/example/ecommerce/Service/ProductService.java
@@ -1,5 +1,6 @@
 package com.example.ecommerce.Service;
 
+import com.example.ecommerce.Dto.CategoryResponse;
 import com.example.ecommerce.Dto.CreateProductRequest;
 import com.example.ecommerce.Dto.ProductResponse;
 import com.example.ecommerce.Model.Category;
@@ -18,7 +19,10 @@ public class ProductService {
     private final ProductRepository productRepository;
     private final CategoryRepository categoryRepository;
     public ProductResponse addProduct(CreateProductRequest request){
-        List<Category> categories = categoryRepository.findById(request.getCategoryIds()).orElseThrow(()-> new RuntimeException("couldn't find the categories"));
+        List<Category> categories = request.getCategoryIds().stream()
+                .map(id->{
+                      return  categoryRepository.findById(id).orElseThrow(()->new RuntimeException("category doesnt found"))
+    }).toList();
         Product product = Product.builder()
                 .name(request.getName())
                 .description(request.getDescription())
@@ -27,8 +31,23 @@ public class ProductService {
                 .categories(categories)
                 .stockQuantity(request.getQuantity())
                 .build();
-        productRepository.save(product);
-        
+       Product saved = productRepository.save(product);
+       List<CategoryResponse> categoryResponses = saved.getCategories().stream()
+               .map(c-> new CategoryResponse(c.getId(),c.getName()))
+               .toList();
+       return new ProductResponse(
+               saved.getId(),
+               saved.getName(),
+               saved.getDescription(),
+               saved.getStockQuantity(),
+               saved.getPrice(),
+               categoryResponses
+       );
+        }
+        public void deleteProduct(Long id){
+        Product product = productRepository.findById(id).orElseThrow(()-> new RuntimeException("product doesnt found"));
+        productRepository.delete(product);
     }
+    
 
 }
